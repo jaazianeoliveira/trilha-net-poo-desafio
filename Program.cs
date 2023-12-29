2f55f561344450f617e3ad962d173c3b75e80989
Mathias Radlov Soares <jaazianeoliveira2017@gmail.com>
2023-12-28 21:19:34 -0300

    Implementação inicial do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone nokia = new Nokia("123456789", "Nokia", "3310");
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Smartphone iphone = new Iphone("987654321", "Apple", "iPhone 12");
+            iphone.Ligar();
+            iphone.InstalarAplicativo("WhatsApp");
+        }
+    }
+}
